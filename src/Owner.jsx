b5db90a9a9cb2caeff5692a406224edcc6d21eb1
Boom_JSD <boom@live.se>
2023-04-18 17:35:47 +0700

    owner image src change

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -1,12 +1,12 @@
 import Layout from "./Layout";
-
+import profilepic from './assets/img/Boom.jpg'
 const Owner = () => {
 
     return (
       <Layout>
           <div>
             <h1>Boom - Group G2 - 53</h1>
-            <img src='./assets/img/Boom.jpg' style={{width:"20%",height:"20%"}}/>
+            <img src={profilepic} style={{width:"20%",height:"20%"}}/>
             <h5>Short Biography:</h5>
             <p>NongBooMzz So cute and lnwza007</p>
           </div>
