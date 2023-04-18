d51dbbc35ddceeb1166eb368c76f86a18581a5ae
Boom_JSD <boom@live.se>
2023-04-18 17:31:55 +0700

    owner image src change

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -6,7 +6,7 @@ const Owner = () => {
       <Layout>
           <div>
             <h1>Boom - Group G2 - 53</h1>
-            <img src='src\assets\img\Boom.jpg' style={{width:"20%",height:"20%"}}/>
+            <img src='./assets/img/Boom.jpg' style={{width:"20%",height:"20%"}}/>
             <h5>Short Biography:</h5>
             <p>NongBooMzz So cute and lnwza007</p>
           </div>
